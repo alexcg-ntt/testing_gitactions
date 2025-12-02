2f0f922935cede45db35c57c28f90d08c20a3bf9
alexcg_ntt <acebriag@emeal.nttdata.com>
2025-12-02 16:45:13 +0100

    openai app settings

--- function_app.py
+++ function_app.py
@@ -1,8 +1,8 @@
 import azure.functions as func
 import logging
 import os
-from openai_access import try_openai
-
+#from openai_access import try_openai
+from openai import AzureOpenAI
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 @app.route(route="test_githubactions_func")
