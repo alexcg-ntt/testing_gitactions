ae4c1dee75d848d7baffa094f96abe622413fd52
alexcg-ntt <acebriag@emeal.nttdata.com>
2025-12-04 17:19:19 +0100

    try imports

--- function_app.py
+++ function_app.py
@@ -2,7 +2,8 @@ import azure.functions as func
 import logging
 import os
 #from openai_access import try_openai
-from openai import AzureOpenAI
+#from openai import AzureOpenAI
+import pandas as pd
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 @app.route(route="test_githubactions_func")
